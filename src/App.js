89e6fb81072daab73be43783714458ee1a356c6b
jerrychibuokem-coursera <jerry.chibuokem@cloud.coursera.org>
2022-09-02 23:05:18 +0100

    setup courier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { CourierProvider } from "@trycourier/react-provider";
+import { Toast } from "@trycourier/react-toast";
+import { Inbox } from "@trycourier/react-inbox";
+import "./App.css";
 
-function App() {
+const userId = Math.round(Math.random() * 10e16).toString(36);
+
+function App({ children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CourierProvider
+      clientKey={process.env.REACT_APP_COURIER_CLIENT_KEY}
+      userId={userId}
+    >
+      <Toast />
+      <Inbox />
+      {children}
+    </CourierProvider>
   );
 }
 
